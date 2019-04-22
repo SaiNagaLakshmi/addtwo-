bdee7eba149a0e3e7eb991452fefacd1e440839b
SaiNagaLakshmi <49276955+SaiNagaLakshmi@users.noreply.github.com>
2019-04-22 02:17:34 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+function clickMe() { 
+    var x = document.createElement("p"); 
+    var t = document.createTextNode("hai sai how are you"); 
+    x.appendChild(t); 
+    document.body.appendChild(x); 
+} 
